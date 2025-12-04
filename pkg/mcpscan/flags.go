32a19ae8dcb2221f71da5b2333ee95606822579a
Antoine Arlaud <antoine@snyk.io>
2025-12-04 15:54:30 +0100

    fix: use alternative key for tenant id env var override

--- pkg/mcpscan/flags.go
+++ pkg/mcpscan/flags.go
@@ -3,18 +3,17 @@ package mcpscan
 import "github.com/spf13/pflag"
 
 const (
-	flagSetName = "mcp-scan"
-)
-
-const (
+	flagSetName      = "mcp-scan"
 	FlagExperimental = "experimental"
+	FlagClientID     = "client-id"
+	FlagTenantID     = "tenant-id"
 )
 
 func getFlagSet() *pflag.FlagSet {
 	flagSet := pflag.NewFlagSet(flagSetName, pflag.ExitOnError)
 	flagSet.Bool(FlagExperimental, false, "This is an experiment feature that will contain breaking changes in future revisions")
-	flagSet.String("client-id", "", "Client ID")
-	flagSet.String("tenant-id", "", "Tenant ID")
+	flagSet.String(FlagClientID, "", "Client ID")
+	flagSet.String(FlagTenantID, "", "Tenant ID")
 
 	return flagSet
 }
