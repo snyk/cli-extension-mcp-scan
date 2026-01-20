0eea0566625a1e8b68857896930b0c97d0cb7f91
aarlaud <antoine@snyk.io>
2026-01-20 15:29:32 +0100

    fix: bump binary version and handle json option (#11)

--- pkg/mcpscan/flags.go
+++ pkg/mcpscan/flags.go
@@ -7,6 +7,7 @@ const (
 	FlagExperimental = "experimental"
 	FlagClientID     = "client-id"
 	FlagTenantID     = "tenant-id"
+	FlagJSON         = "json"
 )
 
 func getFlagSet() *pflag.FlagSet {
@@ -14,6 +15,6 @@ func getFlagSet() *pflag.FlagSet {
 	flagSet.Bool(FlagExperimental, false, "This is an experiment feature that will contain breaking changes in future revisions")
 	flagSet.String(FlagClientID, "", "Client ID")
 	flagSet.String(FlagTenantID, "", "Tenant ID")
-
+	flagSet.Bool(FlagJSON, false, "Output in JSON format")
 	return flagSet
 }
